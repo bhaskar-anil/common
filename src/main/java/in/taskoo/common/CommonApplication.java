9bc8dbd5465699638f7a5d1db0ebb163938362f8
anil bhaskar <anilbhaskar.cse@gmail.com>
2020-03-26 20:37:25 +0530

    adding component scan to common

--- src/main/java/in/taskoo/common/CommonApplication.java
+++ src/main/java/in/taskoo/common/CommonApplication.java
@@ -2,8 +2,10 @@ package in.taskoo.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "in.taskoo" })
 public class CommonApplication {
 
 	public static void main(String[] args) {
